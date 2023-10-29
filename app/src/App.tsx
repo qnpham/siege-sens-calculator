005fa8070f1bc0e0959b4b5e8416a47e576e9e9a
unknown <quannampham@gmail.com>
2023-10-28 17:37:35 -0700

    created inputs for scope

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,56 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <main className="bg-amber-100 h-screen font-source">
-      <h1 className="text-center pt-5 text-2xl font-semibold">ZSC</h1>
+    <main className="bg-amber-100 h-screen font-source ">
+      <div className="header">
+        <h1 className="text-center pt-4 text-3xl font-semibold">ZSC</h1>
+      </div>
+      <div className="flex flex-wrap justify-around pt-10">
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="1x"
+        />
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="1.5x"
+        />
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="2x"
+        />
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="2.5x"
+        />
+      </div>
+      <div className="flex flex-wrap justify-around pt-8">
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="3x"
+        />
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="4x"
+        />
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="5x"
+        />
+        <input
+          type="text"
+          className="rounded w-14 bg-slate-200 text-center"
+          placeholder="12x"
+        />
+      </div>
     </main>
   );
 }
